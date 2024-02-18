8d6be68003411e3607c0314b8148f8662036f741
DR02 <02diegorf@gmail.com>
2024-02-18 19:21:03 +0100

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -2,44 +2,42 @@ import discord
 from discord.ext import commands
 import json
 import youtube_dl
-import random
 
-#Config
+# Configuración
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
     TOKEN = config['token']
     PREFIX = config['prefix']
 
+# Configurar intents
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Crear bot
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-#Commands
-class Music(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+# Comando para reproducir música
+@bot.command()
+async def play(ctx, *, url):
+    channel = ctx.author.voice.channel
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
 
-    @commands.command()
-    async def play(self, ctx, *, url):
-        channel = ctx.author.voice.channel
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+    if voice_client is None:
+        voice_client = await channel.connect()
 
-        if voice_client is None:
-            voice_client = await channel.connect()
+    ydl_opts = {'format': 'bestaudio'}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        voice_client.play(discord.FFmpegPCMAudio(url2))
 
-        ydl_opts = {'format': 'bestaudio'}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            voice_client.play(discord.FFmpegPCMAudio(url2))
-
-    @commands.command()
-    async def stop(self, ctx):
-        voice_client = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
-        if voice_client and voice_client.is_playing():
-            voice_client.stop()
-            await ctx.send("La música ha sido detenida.")
+# Comandos adicionales
+@bot.command()
+async def stop(ctx):
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice_client and voice_client.is_playing():
+        voice_client.stop()
+        await ctx.send("La música ha sido detenida.")
 
 @bot.command()
 async def frases(ctx):
@@ -56,11 +54,10 @@ async def frases(ctx):
 async def pxg(ctx):
     await ctx.send("**FRESCOOOO!**")  
 
+# Evento para imprimir un mensaje en la consola cuando el bot está listo
 @bot.event
 async def on_ready():
     print('Bot is ready.')
 
-bot.add_cog(Music(bot))
-
-#Start Bot
+# Ejecutar el bot
 bot.run(TOKEN)
